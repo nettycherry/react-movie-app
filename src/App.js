27006d8cd25135cdb86e4626a4ed06e4c00a3300
nettycherry <nettycherry@users.noreply.github.com>
2021-02-06 02:39:45 +0900

    #7. setState

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ state ={
   count:0 //디폴트값 0을 가짐
 }
 
-add = () => {console.log("add")}
-minus = () => {console.log("minus")}
+add = () => {
+  this.setState({count:1})}
+  //state 직접 변경하지 말고 setState를 사용하라. 그럼 state와 render 함수를 둘 다 refresh 해줌.(new state와 함께 render 재호출)  
+minus = () => {
+  this.setState({count:-1})}
 
 //react component는 render method를 가지고 있고, App component는 react component에서 확장했기 때문에 역시 render method를 가지고 있다.
 //react는 자동으로 class component의 render method를 실행한다
@@ -27,7 +30,7 @@ minus = () => {console.log("minus")}
       {/*class라서 그냥 {state.count}라고 하면 안 되고 this를 붙여줘야 함*/}
       <button onClick={this.add}>Add</button>
       <button onClick={this.minus}>Minus</button>
-      {/*react는 자동으로 onClick prop이 주어짐. js에서는 onClick이나 eventListener를 등록해야 함.
+      {/*react에선 자동으로 onClick prop이 주어짐. js에서는 onClick이나 eventListener를 등록해야 함.
       add() 이렇게 쓰지 않음. ()는 '즉시 호출'을 의미함. 여기선 즉시가 아니라 click 됐을 때만 함수가 실행되어야 하므로 () 쓰면 안 됨!*/}
     </div>)
     // div 쓰면? 두 줄 이상이면? return 뒤에 () 붙여줘야 함

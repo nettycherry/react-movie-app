a37b0fe18d6d05299351c6e1d41eb1db12a178f0
nettycherry <nettycherry@users.noreply.github.com>
2021-02-05 15:27:24 +0900

    make an array by using jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+function Food ({name}) {
+  return <h3>I love {name}</h3>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div> 
+    <h1>hello!</h1> 
+    
+    <Food name="kimchi" />
+    <Food name="ramen" />
+
     </div>
-  );
+  ;
 }
 
 export default App;
+
+/*component에 html 형식으로 name 과 같은 데이터 저장 가능
+food 라는 component에 name이라는 이름의 prop(erty)에 kimchi라는 value를 부여
+props(propertys)의 이름을 component의 매개변수 안에 {} 안에 넣어서 데이터를 가져올 수 있다*/
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ./는 같은 directory에 있다는 뜻
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App />, 
+    /*react component -> App.js에서 알 수 있듯 
+    react component는 HTML을 반환하는 함수다.
+    이렇게 JS랑 HTML을 조합하는 리액트의 개념을 JSX라고 한다?
+    다시, jsx는 js+html, 즉 javascript 안의 html이고 component를 사용한다.*/
+
+    /*react application은 하나의 component만 rendering 해야 하기 때문에
+    Potato component는 App component 안에 넣어준다 */
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
